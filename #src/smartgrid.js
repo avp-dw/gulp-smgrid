117704b0d7cffc4ebc1d6c508ac56eabb7656343
avp-dw <ya.stily@gmail.com>
2020-10-07 09:35:36 +0500

    gulp smart grid

---
+++ #src/smartgrid.js
@@ -0,0 +1,25 @@
+module.exports = {
+    outputStyle: 'scss', 
+    columns: 12, 
+    offset: '30px', 
+    mobileFirst: false, 
+    container: {
+        maxWidth: '1200px', 
+        fields: '30px' 
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', 
+        },
+        md: {
+            width: '960px'
+        },
+        sm: {
+            width: '780px',
+            fields: '15px' 
+        },
+        xs: {
+            width: '560px'
+        }
+    }    
+};
